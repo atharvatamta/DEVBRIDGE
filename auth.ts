e4ba8df01da4dd55e22c7459f39c3490e9868abb
atharva <atharva.tamta@ikarus3d.com>
2026-03-12 18:04:16 +0530

    "init"

---
+++ auth.ts
@@ -0,0 +1,22 @@
+import GitHub from "next-auth/providers/github";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [GitHub],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
---
+++ server/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../lib/prisma.server"; 
+import authConfig from "../auth";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  adapter: PrismaAdapter(prisma),
+  ...authConfig,
+});
